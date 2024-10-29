407c0dac2cac1566e6d57dd2d3e3d49cd10a575e
jmamula <jakemamula@gmail.com>
2024-10-29 18:08:06 -0400

    the change to startup.cs that david helped with

--- ONYX-RAIN.Api/Startup.cs
+++ ONYX-RAIN.Api/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ONYX-RAIN.Api
+namespace ONYX.RAIN.Api
 {
     public class Startup
     {
